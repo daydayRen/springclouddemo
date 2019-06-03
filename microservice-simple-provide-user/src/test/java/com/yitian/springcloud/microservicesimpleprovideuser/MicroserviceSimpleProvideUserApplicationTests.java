3a5990629dcd04be9e90b956b6a84079868ea7e0
daydayRen <1209050193@qq.com>
2019-06-03 18:41:15 +0800

    Initial commit  微服务小demo

---
+++ microservice-simple-provide-user/src/test/java/com/yitian/springcloud/microservicesimpleprovideuser/MicroserviceSimpleProvideUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yitian.springcloud.microservicesimpleprovideuser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceSimpleProvideUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
